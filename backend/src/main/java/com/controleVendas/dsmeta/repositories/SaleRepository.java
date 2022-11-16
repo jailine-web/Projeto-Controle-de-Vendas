51cd842bd0fb0234e604ff9926c0a289e2d05069
Jailine <jay.santana964@gmail.com>
2022-11-16 20:11:45 -0300

    Testando a API

---
+++ backend/src/main/java/com/controleVendas/dsmeta/repositories/SaleRepository.java
@@ -0,0 +1,9 @@
+package com.controleVendas.dsmeta.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.controleVendas.dsmeta.entities.Sale;
+
+public interface SaleRepository extends JpaRepository<Sale, Long>{
+
+}
---
+++ backend/src/main/java/com/controleVendas/dsmeta/services/SaleService.java
@@ -0,0 +1,21 @@
+package com.controleVendas.dsmeta.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.controleVendas.dsmeta.entities.Sale;
+import com.controleVendas.dsmeta.repositories.SaleRepository;
+
+@Service
+public class SaleService {
+
+	@Autowired
+	private SaleRepository repository;
+	
+	public List<Sale> findSales() {
+		
+		return repository.findAll();
+	}
+}
