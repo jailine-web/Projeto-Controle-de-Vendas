5e9dd7d2c3e77d8d37ef60e721755e04c1a1f792
Jailine <jay.santana964@gmail.com>
2022-11-19 18:55:14 -0300

    Lista de vendas

--- frontend/src/Componentes/SalesCard/index.tsx
+++ frontend/src/Componentes/SalesCard/index.tsx
@@ -2,110 +2,93 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import { Sale } from "../../assets/models/sale";
+import { BASE_URL } from "../../utils/request";
 import BotaoNotificacao from '../botaoNotificacao';
 import './index.css';
 
-function SalesCard(){
+function SalesCard() {
 
   const date = new Date(new Date().setDate(new Date().getDate() - 365));
 
   const max = new Date();
 
-    const [minDate, setMinDate] = useState(date);
-    const [maxDate, setMaxDate] = useState(max);
+  const [minDate, setMinDate] = useState(date);
+  const [maxDate, setMaxDate] = useState(max);
 
-    useEffect (()=> {
-      axios.get("http://localhost:8080/sales").then(response => {
-        console.log(response.data);
-      })
-      
-    }, []);
+  const [sales, setSales] = useState<Sale[]>([]);
+
+  useEffect(() => {
+    axios.get(`${BASE_URL}/sales`).then(response => {
+      setSales(response.data.content);
+    })
+
+  }, []);
 
 
-    return(
-        <div className="dsmeta-card">
-        <h2 className="dsmeta-sales-title">Relatório de vendas</h2>
-        <div>
-          <div className="dsmeta-form-control-container">
+  return (
+    <div className="dsmeta-card">
+      <h2 className="dsmeta-sales-title">Relatório de vendas</h2>
+      <div>
+        <div className="dsmeta-form-control-container">
 
           <h3>Data inicial:</h3>
           <DatePicker
             selected={minDate}
-            onChange={(date: Date) => {setMinDate(date)}}
+            onChange={(date: Date) => { setMinDate(date) }}
             className="dsmeta-form-control"
-            dateFormat="dd/MM/yyyy"/>
-          </div>
-          <div className="dsmeta-form-control-container">
+            dateFormat="dd/MM/yyyy" />
+        </div>
+        <div className="dsmeta-form-control-container">
 
           <h3>Data final:</h3>
           <DatePicker
             selected={maxDate}
-            onChange={(date: Date) => {setMaxDate(date)}}
+            onChange={(date: Date) => { setMaxDate(date) }}
             className="dsmeta-form-control"
             dateFormat="dd/MM/yyyy"
-            />
-          </div>
+          />
         </div>
-
-        <div>
-          <table className="dsmeta-sales-table">
-            <thead>
-              <tr>
-                <th className="show992">ID</th>
-                <th className="show576">Data</th>
-                <th>Vendedor</th>
-                <th className="show992">Visitas</th>
-                <th className="show992">Vendas</th>
-                <th>Total</th>
-                <th>Notificar</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td className="show992">#341</td>
-                <td className="show576">08/07/2022</td>
-                <td>Anakin</td>
-                <td className="show992">15</td>
-                <td className="show992">11</td>
-                <td>R$ 55300.00</td>
-                <td>
-                  <div className="dsmeta-red-btn-container">
-                    <BotaoNotificacao/>
-                  </div>
-                </td>
-              </tr>
-              <tr>
-                <td className="show992">#341</td>
-                <td className="show576">08/07/2022</td>
-                <td>Anakin</td>
-                <td className="show992">15</td>
-                <td className="show992">11</td>
-                <td>R$ 55300.00</td>
-                <td>
-                  <div className="dsmeta-red-btn-container">
-                    <BotaoNotificacao/>
-                  </div>
-                </td>
-              </tr>
-              <tr>
-                <td className="show992">#341</td>
-                <td className="show576">08/07/2022</td>
-                <td>Anakin</td>
-                <td className="show992">15</td>
-                <td className="show992">11</td>
-                <td>R$ 55300.00</td>
-                <td>
-                  <div className="dsmeta-red-btn-container">
-                    <BotaoNotificacao/>
-                  </div>
-                </td>
-              </tr>
-            </tbody>
-
-          </table>
-        </div>
-
       </div>
-    )
+
+      <div>
+        <table className="dsmeta-sales-table">
+          <thead>
+            <tr>
+              <th className="show992">ID</th>
+              <th className="show576">Data</th>
+              <th>Vendedor</th>
+              <th className="show992">Visitas</th>
+              <th className="show992">Vendas</th>
+              <th>Total</th>
+              <th>Notificar</th>
+            </tr>
+          </thead>
+          <tbody>
+
+            {sales.map(sale => {
+              return (
+                <tr key={sale.id}>
+                  <td className="show992">{sale.id}</td>
+                  <td className="show576">{new Date(sale.date).toLocaleDateString()}</td>
+                  <td>{sale.sellerName}</td>
+                  <td className="show992">{sale.visited}</td>
+                  <td className="show992">{sale.deals}</td>
+                  <td>{sale.amount.toFixed(2)}</td>
+                  <td>
+                    <div className="dsmeta-red-btn-container">
+                      <BotaoNotificacao />
+                    </div>
+                  </td>
+                </tr>
+              )
+            })}
+          </tbody>
+
+        </table>
+      </div>
+
+    </div>
+  )
 }
 export default SalesCard;
